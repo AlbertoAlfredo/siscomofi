e9b4aba6bab572ef58902defc3bf4e819e7ead39
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2025-10-12 13:48:25 -0300

    Inicio de envio do front para o back

--- routes/route_caixa.py
+++ routes/route_caixa.py
@@ -15,6 +15,30 @@ def caixa_banco():
                 'data_lancamento': request.form.data_lancamento,
                 'n_doc': request.form.n_doc,
                 'historico_lancamento': request.form.historico_lancamento
+            },
+            "agenfa": {
+                "data_pagamento": request.form.agenfa_data_pagamento,
+                "data_pagamento": request.form.agenfa_n_cheque,
+                "data_pagamento": request.form.agenfa_descricao_pagamento,
+                "data_pagamento": request.form.agenfa_valor_pagamento
+            },
+            "iagro": {
+                "data_pagamento": request.form.iagro_data_pagamento,
+                "data_pagamento": request.form.iagro_n_cheque,
+                "data_pagamento": request.form.iagro_descricao_pagamento,
+                "data_pagamento": request.form.iagro_valor_pagamento
+            },
+            "pagamentos": {
+                "data_pagamento": request.form.pagamentos_data_pagamento,
+                "data_pagamento": request.form.pagamentos_n_cheque,
+                "data_pagamento": request.form.pagamentos_descricao_pagamento,
+                "data_pagamento": request.form.pagamentos_valor_pagamento
+            },
+            "outros": {
+                "data_pagamento": request.form.outros_data_pagamento,
+                "data_pagamento": request.form.outros_n_cheque,
+                "data_pagamento": request.form.outros_descricao_pagamento,
+                "data_pagamento": request.form.outros_valor_pagamento
             }
         }
     return render_template('lancamentos_caixa_banco.html')
