dcf98dc5fec1b858103df03c7fa0f6a505711777
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2025-11-10 22:23:39 -0300

    relatorio honorarios

--- routes/route_relatorios.py
+++ routes/route_relatorios.py
@@ -3,6 +3,7 @@ import math
 from flask import render_template, request, Blueprint, redirect, url_for
 from datetime import date, datetime
 from routes.route_caixa import Lancamentos as lancamentos
+from routes.route_receitas_despesas import Lancamentos as receitas
 from models.base import get_all, filter_date
 import utils
 
@@ -29,3 +30,21 @@ def relatorios_receitadespesa():
     else:
         relatorio = get_all(lancamentos, order_by=lancamentos.data_lancamento)
     return render_template("relatorios/relatorio_receitas_despesas.html", receitas=relatorio, utils=utils)
+
+@relatorios_bp.route('/relatorios/honorarios_taxas', methods=['GET'])
+def relatorios_honorarios():
+    data_inicio = datetime.strptime(request.args.get('data_inicio'), '%Y-%m-%d').date() if request.args.get('data_inicio') else False
+    data_fim = datetime.strptime(request.args.get('data_fim'), '%Y-%m-%d').date() if request.args.get('data_fim') else False
+    if data_inicio and data_fim and data_inicio > data_fim:
+        return render_template("relatorios/rel_honorarios_taxas.html", erro=True, utils=utils)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, data_inicio, data_fim)
+    elif data_inicio:
+        relatorio = filter_date(receitas, receitas.data_lancamento, date_inicio=data_inicio, date_fim=False)
+    elif data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, date_inicio=False, date_fim=data_fim)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, data_inicio, data_fim )
+    else:
+        relatorio = get_all(receitas, order_by=receitas.data_lancamento)
+    return render_template("relatorios/rel_honorarios_taxas.html", receitas=relatorio, utils=utils)
