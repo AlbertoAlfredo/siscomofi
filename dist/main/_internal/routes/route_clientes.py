4d701f24739cf9cc1d00100aab24c120a8ec0742
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2025-11-18 17:54:00 -0300

    executável beta em teste

---
+++ dist/main/_internal/routes/route_clientes.py
@@ -0,0 +1,106 @@
+import math
+from flask import Flask, render_template, request, Blueprint, redirect, url_for
+from sqlalchemy import Column, Integer, String, Float
+import models.base as db
+from models.base import Base
+
+cliente_bp = Blueprint(
+                        'cliente_bp',
+                             __name__,
+                             static_folder='static',
+                             template_folder='templates'
+                             )
+
+class Clientes(Base):
+    __tablename__ = "clientes"
+
+    id = Column(Integer, primary_key=True)
+    nome_cliente = Column(String(100), nullable=False)
+    nome_propriedade = Column(String(100))
+    endereco = Column(String(150))
+    numero = Column(String(10))
+    bairro = Column(String(50))
+    cidade = Column(String(50))
+    uf = Column(String(2))
+    tipo_pessoa = Column(String(10))
+    cpf_cnpj = Column(String(18))
+    inscricao_estadual = Column(String(20))
+    telefone = Column(String(15))
+    celular = Column(String(15))
+    valor_honorario = Column(Integer)
+    observacoes = Column(String(500))
+
+    def to_dict(self):
+        """Converte o objeto Cliente num dicionário."""
+        # Usa __table__.columns para obter dinamicamente todas as colunas
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+@cliente_bp.route("/clientes-cadastro", methods=['GET', 'POST'])
+def clientes_cadastro():
+    if request.method == "POST":
+
+        cliente = {
+            "nome_cliente": request.form["nome"],
+            "nome_propriedade": request.form["npropriedade"],
+            "endereco": request.form["endereco"],
+            "numero": request.form["numero"],
+            "bairro": request.form["bairro"],
+            "cidade": request.form["cidade"],
+            "uf": request.form["uf"],
+            "tipo_pessoa": request.form["tipopessoa"],
+            "cpf_cnpj": request.form["cpf_cnpj"],
+            "inscricao_estadual": request.form["inscricao_estadual"],
+            "telefone": request.form["telefone"],
+            "celular": request.form["celular"],
+            "valor_honorario": request.form["valorhonorario"],
+            "observacoes": request.form["observacoes"],
+        }
+        db.add(Clientes, cliente)
+        return render_template("clientes-cadastro.html")
+    elif request.method == 'GET' and request.args.get('id'):
+        cliente = db.get_for_id(Clientes, request.args.get('id'))
+        return render_template("clientes-cadastro.html", cliente = cliente)
+    else:
+        return render_template("clientes-cadastro.html")
+    
+@cliente_bp.route("/clientes-atualiza", methods=['GET', 'POST'])  
+def clientes_atualiza():
+    cliente = {
+        "id": request.form.get('id'),
+        "nome_cliente": request.form.get("nome"),
+        "nome_propriedade": request.form.get("npropriedade"),
+        "endereco": request.form.get("endereco"),
+        "numero": request.form.get("numero"),
+        "bairro": request.form.get("bairro"),
+        "cidade": request.form.get("cidade"),
+        "uf": request.form.get("uf"),
+        "tipo_pessoa": request.form.get("tipopessoa"),
+        "cpf_cnpj": request.form.get("cpfcnpj"),
+        "inscricao_estadual": request.form.get("iestadual"),
+        "telefone": request.form.get("fone"),
+        "celular": request.form.get("celular"),
+        "valor_honorario": request.form.get("valorhonorario"),
+        "observacoes": request.form.get("observacoes"),
+    }
+    db.update(Clientes, cliente['id'], cliente)
+    return redirect(url_for('cliente_bp.clientes_lista'))
+
+@cliente_bp.route("/cliente_apagar", methods=['GET'])
+def cliente_apagar():
+    cliente_id = request.args.get('id')
+    db.delete(Clientes, cliente_id)
+    return redirect(url_for('cliente_bp.clientes_lista'))
+    
+@cliente_bp.route("/clientes_lista", methods=['GET'])
+def clientes_lista():
+    page = request.args.get('page', 1, type=int)
+    ITENS_POR_PAGINA = 10
+    clientes_da_pagina = db.get_paginate(Clientes, Clientes.nome_cliente, page, ITENS_POR_PAGINA)
+    total_clientes = db.count_total(Clientes)
+    total_paginas = math.ceil(total_clientes / ITENS_POR_PAGINA)
+
+    return render_template("clientes-lista.html",
+                           clientes = clientes_da_pagina,
+                           pagina_atual = page,
+                           total_paginas = total_paginas
+                           )
---
+++ dist/main/_internal/routes/route_relatorios.py
@@ -0,0 +1,78 @@
+import math
+
+from flask import render_template, request, Blueprint, redirect, url_for
+from datetime import date, datetime
+from routes.route_caixa import Lancamentos as lancamentos
+from routes.route_receitas_despesas import Lancamentos as receitas
+from routes.route_clientes import Clientes as clientes
+from models.base import get_all, filter_date, get_busca
+import utils
+
+relatorios_bp = Blueprint(
+                    'relatorios',
+                          __name__,
+                          template_folder='templates'
+                          )
+
+@relatorios_bp.route('/relatorios/saldo', methods=['GET'])
+def relatorios_receitadespesa():
+    data_inicio = datetime.strptime(request.args.get('data_inicio'), '%Y-%m-%d').date() if request.args.get('data_inicio') else False
+    data_fim = datetime.strptime(request.args.get('data_fim'), '%Y-%m-%d').date() if request.args.get('data_fim') else False
+    if data_inicio and data_fim and data_inicio > data_fim:
+        return render_template("relatorios/rel_saldo.html", erro=True, utils=utils)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(lancamentos, lancamentos.data_lancamento, data_inicio, data_fim)
+    elif data_inicio:
+        relatorio = filter_date(lancamentos, lancamentos.data_lancamento, date_inicio=data_inicio, date_fim=False)
+    elif data_fim:
+        relatorio = filter_date(lancamentos, lancamentos.data_lancamento, date_inicio=False, date_fim=data_fim)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(lancamentos, lancamentos.data_lancamento, data_inicio, data_fim )
+    else:
+        relatorio = get_all(lancamentos, order_by=lancamentos.data_lancamento)
+    return render_template("relatorios/rel_saldo.html", receitas=relatorio, utils=utils)
+
+@relatorios_bp.route('/relatorios/honorarios_taxas', methods=['GET'])
+def relatorios_honorarios():
+    data_inicio = datetime.strptime(request.args.get('data_inicio'), '%Y-%m-%d').date() if request.args.get('data_inicio') else False
+    data_fim = datetime.strptime(request.args.get('data_fim'), '%Y-%m-%d').date() if request.args.get('data_fim') else False
+    if data_inicio and data_fim and data_inicio > data_fim:
+        return render_template("relatorios/rel_taxas.html", erro=True, utils=utils)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, data_inicio, data_fim)
+    elif data_inicio:
+        relatorio = filter_date(receitas, receitas.data_lancamento, date_inicio=data_inicio, date_fim=False)
+    elif data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, date_inicio=False, date_fim=data_fim)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, data_inicio, data_fim )
+    else:
+        relatorio = get_all(receitas, order_by=receitas.data_lancamento)
+    return render_template("relatorios/rel_taxas.html", receitas=relatorio, utils=utils)
+
+@relatorios_bp.route("/relatorios/resumo-geral", methods=["GET"])
+def resumo_geral():
+    data_inicio = datetime.strptime(request.args.get('data_inicio'), '%Y-%m-%d').date() if request.args.get('data_inicio') else False
+    data_fim = datetime.strptime(request.args.get('data_fim'), '%Y-%m-%d').date() if request.args.get('data_fim') else False
+    if data_inicio and data_fim and data_inicio > data_fim:
+        return render_template("relatorios/rel_resumo_geral.html", erro=True, utils=utils)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, data_inicio, data_fim)
+    elif data_inicio:
+        relatorio = filter_date(receitas, receitas.data_lancamento, date_inicio=data_inicio, date_fim=False)
+    elif data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, date_inicio=False, date_fim=data_fim)
+    elif data_inicio and data_fim:
+        relatorio = filter_date(receitas, receitas.data_lancamento, data_inicio, data_fim )
+    else:
+        relatorio = get_all(receitas, order_by=receitas.data_lancamento)
+    return render_template("relatorios/rel_resumo_geral.html", receitas=relatorio, utils=utils)
+
+@relatorios_bp.route("/relatorios/honorarios", methods=["GET"])
+def honorarios():
+    busca = request.args.get('busca')
+    if busca:
+        relatorio = get_busca(clientes, busca, [clientes.nome_cliente, clientes.nome_propriedade, clientes.cpf_cnpj])
+    else:
+        relatorio = get_all(clientes, order_by=clientes.nome_cliente)
+    return render_template("relatorios/honorarios.html", clientes=relatorio, utils=utils)
